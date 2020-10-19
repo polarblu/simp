6d555652f2ae6f48fe88dd97938cdcbed97b44cd
polarblu <72341041+polarblu@users.noreply.github.com>
2020-10-19 13:50:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 
+	// Include Database class here.
+
 	// Bitcoin wallet
 
 	// The index.php file
 	printf("The index.php file for this project. This file will handle all calls and outputs.");
+
+	
+
 ?>
